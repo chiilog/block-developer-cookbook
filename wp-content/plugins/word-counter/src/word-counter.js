12abca9c99115e50629983807c6a0154bc5d91e1
chiilog <purgexpurge@gmail.com>
2024-04-01 15:35:41 +0900

    文字数未満で公開できないようにする

--- wp-content/plugins/word-counter/src/word-counter.js
+++ wp-content/plugins/word-counter/src/word-counter.js
@@ -1,26 +1,63 @@
-/*
+/**
  * WordPress dependencies
  */
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginPostStatusInfo } from '@wordpress/edit-post';
-import { useSelect } from '@wordpress/data';
+import { useSelect, useDispatch } from '@wordpress/data';
 import { store as blockEditorStore } from '@wordpress/block-editor';
 import { count } from '@wordpress/wordcount';
 import { serialize } from '@wordpress/blocks';
+import { useEffect, useState } from '@wordpress/element';
+import { store as editorStore } from '@wordpress/editor';
+
+/**
+ * Internal dependencies
+ */
+import WordCountDisplayComponent from './components/wordCountDisplay';
 
 const Render = () => {
-	// Get the blocks from the editor.
+	// Get the blocks from the editor.,
 	const blocks = useSelect(
 		( select ) => select( blockEditorStore ).getBlocks(),
 		[]
 	);
+	// Get the lockPostSaving and unlockPostSaving functions from the editor
+	const { lockPostSaving, unlockPostSaving } = useDispatch( editorStore );
 
-	// 日本語をカウントしたいので、characters_excluding_spaces を使う
-	const numberOfWordsInContent = count( serialize( blocks ), 'characters_excluding_spaces' );
+	// Define the word count display state.
+	const [ wordCountDisplay, setWordCountDisplay ] = useState( 0 );
+
+	// Track the changes in the content
+	useEffect( () => {
+		// Define a variable to track whether the post should be locked
+		let lockPost = false;
+
+		// Get the WordCount
+		const currentWordCount = count( serialize( blocks ), 'characters_excluding_spaces' );
+		setWordCountDisplay( currentWordCount );
+
+		// If the word count is less than the required, lock the post saving.
+		if ( currentWordCount < requiredWordCount ) {
+			lockPost = true;
+		}
+
+		// Lock or enable saving.
+		if ( lockPost === true ) {
+			lockPostSaving();
+		} else {
+			unlockPostSaving();
+		}
+	}, [ blocks, lockPostSaving, unlockPostSaving ] );
+
+	// The required number of words;
+	const requiredWordCount = 10;
 
 	return (
 		<PluginPostStatusInfo>
-			{ `There are ${ numberOfWordsInContent } words in the content` }
+			<WordCountDisplayComponent
+				wordCount={ wordCountDisplay }
+				required={ requiredWordCount }
+			/>
 		</PluginPostStatusInfo>
 	);
 };
